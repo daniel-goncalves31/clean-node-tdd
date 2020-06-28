c1568b2ad4c0d32394885d0b191a09d93519b8ae
Daniel Gonçalves <danielgg0831@gmail.com>
2020-06-28 16:42:30 -0300

    test: ensure LoadUserByEmailRepository throws if no userModel is provided

--- src/infra/repositories/load-user-by-email-repository.js
+++ src/infra/repositories/load-user-by-email-repository.js
@@ -4,6 +4,9 @@ module.exports = class LoadUserByEmailRepository {
   }
 
   async load (email) {
+    if (!this.userModel) {
+      throw new Error('UserModel undefined')
+    }
     const user = await this.userModel.findOne({ email }, {
       projection: {
         password: 1
