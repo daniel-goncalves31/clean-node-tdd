4625a659a9457a83ebb1fe3dcb19efa8a17d5b2f
Daniel Gonçalves <danielgg0831@gmail.com>
2020-06-28 14:22:22 -0300

    refactor: move LoadUserByEmailRepository creation to a helper factory method

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -10,9 +10,18 @@ class LoadUserByEmailRepository {
     return user
   }
 }
+let client, db
+
+const makeSut = () => {
+  const userModel = db.collection('users')
+  const sut = new LoadUserByEmailRepository(userModel)
+  return {
+    sut,
+    userModel
+  }
+}
 
 describe('LoadUserByEmailRepository', () => {
-  let client, db
   beforeAll(async () => {
     client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
@@ -29,18 +38,16 @@ describe('LoadUserByEmailRepository', () => {
     await client.close()
   })
   test('should return null if no user is found', async () => {
-    const userModel = db.collection('users')
-    const sut = new LoadUserByEmailRepository(userModel)
+    const { sut } = makeSut()
     const user = await sut.load('valid_email@email.com')
     expect(user).toBeNull()
   })
   test('should return an user if user is found', async () => {
-    const userModel = db.collection('users')
+    const { sut, userModel } = makeSut()
     const email = 'valid_email@email.com'
     await userModel.insertOne({
       email
     })
-    const sut = new LoadUserByEmailRepository(userModel)
     const user = await sut.load(email)
     expect(user.email).toBe(email)
   })
